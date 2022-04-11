5d394bf361352684863e175946c3d54a0cbb6226
chamskra66er <chamskra66er@gmail.com>
2022-04-11 22:13:27 +0300

    added Task interface

---
+++ PopugJira/Interfaces/ITask.cs
@@ -0,0 +1,13 @@
+﻿using PopugJira.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PopugJira.Interfaces
+{
+    public interface ITask
+    {
+        Task AddTask(Tasks task);
+        Task RemoveTask(Tasks task);
+        IEnumerable<Tasks> TakList();
+    }
+}
